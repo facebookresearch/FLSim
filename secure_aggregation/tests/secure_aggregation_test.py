19bbcebc4b6493712b7d168fc79606d64a01631d
Jesik Min <jesikmin@fb.com>
2021-11-04 11:20:58 -0700

    Update secure_aggregation_test as a PyTest
    Summary: as title
    
    Differential Revision: D32112706
    
    fbshipit-source-id: 215af26bc4a5392cc7d6b4cc64bdd0cc9c0e68f8

--- secure_aggregation/tests/secure_aggregation_test.py
+++ secure_aggregation/tests/secure_aggregation_test.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
-import unittest
-
 import torch
+from flsim.common.pytest_helper import assertEqual, assertTrue, assertRaises
 from flsim.secure_aggregation.secure_aggregator import (
     FixedPointConverter,
     SecureAggregator,
@@ -14,7 +13,7 @@ from flsim.tests import utils
 from omegaconf import OmegaConf
 
 
-class SecureAggregatorTest(unittest.TestCase):
+class TestSecureAggregator:
     def _create_model(self, model_param_value):
         """
         Creates a two-layer model
@@ -31,17 +30,17 @@ class SecureAggregatorTest(unittest.TestCase):
         converter = FixedPointConverter(
             **OmegaConf.structured(FixedPointConfig(num_bytes=2, scaling_factor=1000))
         )
-        self.assertEqual(converter.max_value, 32767)
-        self.assertEqual(converter.min_value, -32768)
+        assertEqual(converter.max_value, 32767)
+        assertEqual(converter.min_value, -32768)
 
-        with self.assertRaises(ValueError):
+        with assertRaises(ValueError):
             converter = FixedPointConverter(
                 **OmegaConf.structured(
                     FixedPointConfig(num_bytes=9, scaling_factor=1000)
                 )
             )
 
-        with self.assertRaises(ValueError):
+        with assertRaises(ValueError):
             converter = FixedPointConverter(
                 **OmegaConf.structured(
                     FixedPointConfig(num_bytes=3, scaling_factor=-100)
@@ -61,37 +60,37 @@ class SecureAggregatorTest(unittest.TestCase):
         x = torch.tensor(17.42)
         y = converter.to_fixedpoint(x)
         # y = x * scaling_factor = 1742.0 ==> round to 1742
-        self.assertEqual(y, torch.tensor(1742))
+        assertEqual(y, torch.tensor(1742))
 
         x = torch.tensor(17.4298)
         y = converter.to_fixedpoint(x)
         # y = x * scaling_factor = 1742.98 ==> round to 1743
-        self.assertEqual(y, torch.tensor(1743))
+        assertEqual(y, torch.tensor(1743))
 
         x = torch.tensor(-2.34)
         y = converter.to_fixedpoint(x)
         # y = x * scaling_factor = -234.0 ==> round to -234
-        self.assertEqual(y, torch.tensor(-234))
+        assertEqual(y, torch.tensor(-234))
 
         x = torch.tensor(-2.3456)
         y = converter.to_fixedpoint(x)
         # y = x * scaling_factor = -234.56 ==> round to -235
-        self.assertEqual(y, torch.tensor(-235))
+        assertEqual(y, torch.tensor(-235))
 
         x = torch.tensor(-2.3416)
         y = converter.to_fixedpoint(x)
         # y = x * scaling_factor = -234.16 ==> round to -234
-        self.assertEqual(y, torch.tensor(-234))
+        assertEqual(y, torch.tensor(-234))
 
         x = torch.tensor(12345.0167)
         y = converter.to_fixedpoint(x)
         # y = x * scaling_factor = 1234501.67 ==> adjust to maxValue 32767
-        self.assertEqual(y, torch.tensor(32767))
+        assertEqual(y, torch.tensor(32767))
 
         x = torch.tensor(-327.69)
         y = converter.to_fixedpoint(x)
         # y = x * scaling_factor = -32769 ==> adjust to minValue -32768
-        self.assertEqual(y, torch.tensor(-32768))
+        assertEqual(y, torch.tensor(-32768))
 
     def test_fixed_to_floating_point(self):
         """
@@ -105,12 +104,12 @@ class SecureAggregatorTest(unittest.TestCase):
         x = torch.tensor(85)
         y = converter.to_float(x)
         # y = x / scaling_factor = 1.0
-        self.assertTrue(torch.allclose(y, torch.tensor(1.0), rtol=1e-10))
+        assertTrue(torch.allclose(y, torch.tensor(1.0), rtol=1e-10))
 
         x = torch.tensor(157)
         y = converter.to_float(x)
         # y = x / scaling_factor = 1.847058823529412
-        self.assertTrue(torch.allclose(y, torch.tensor(1.847058823529412), rtol=1e-10))
+        assertTrue(torch.allclose(y, torch.tensor(1.847058823529412), rtol=1e-10))
 
     def test_params_floating_to_fixedpoint(self):
         """
@@ -125,13 +124,13 @@ class SecureAggregatorTest(unittest.TestCase):
         secure_aggregator = SecureAggregator(utility_config_flatter(model, config))
         secure_aggregator.params_to_fixedpoint(model)
         mismatched = utils.model_parameters_equal_to_value(model, 633.0)
-        self.assertEqual(mismatched, "", mismatched)
+        assertEqual(mismatched, "", mismatched)
 
         model = self._create_model(-3.8345)
         secure_aggregator = SecureAggregator(utility_config_flatter(model, config))
         secure_aggregator.params_to_fixedpoint(model)
         mismatched = utils.model_parameters_equal_to_value(model, -383.0)
-        self.assertEqual(mismatched, "", mismatched)
+        assertEqual(mismatched, "", mismatched)
 
     def test_params_floating_to_fixedpoint_different_config_for_layers(self):
         """
@@ -160,10 +159,10 @@ class SecureAggregatorTest(unittest.TestCase):
         for name, p in model.named_parameters():
             if name == "fc1.weight" or name == "fc1.bias":
                 # round 547.28 to 547
-                self.assertTrue(torch.allclose(p, torch.tensor(547.0), rtol=1e-10))
+                assertTrue(torch.allclose(p, torch.tensor(547.0), rtol=1e-10))
             if name == "fc2.weight" or name == "fc2.bias":
                 # round 54.728 to 55
-                self.assertTrue(torch.allclose(p, torch.tensor(55.0), rtol=1e-10))
+                assertTrue(torch.allclose(p, torch.tensor(55.0), rtol=1e-10))
 
     def test_error_raised_per_layer_config_not_set(self):
         """
@@ -180,10 +179,10 @@ class SecureAggregatorTest(unittest.TestCase):
         model = self._create_model(600)
         secure_aggregator = SecureAggregator(config)
 
-        with self.assertRaises(ValueError):
+        with assertRaises(ValueError):
             secure_aggregator.params_to_float(model)
 
-        with self.assertRaises(ValueError):
+        with assertRaises(ValueError):
             secure_aggregator.params_to_fixedpoint(model)
 
     def test_params_fixed_to_floating_point(self):
@@ -196,7 +195,7 @@ class SecureAggregatorTest(unittest.TestCase):
         secure_aggregator = SecureAggregator(utility_config_flatter(model, config))
         secure_aggregator.params_to_float(model)
         mismatched = utils.model_parameters_equal_to_value(model, 22.0)
-        self.assertEqual(mismatched, "", mismatched)
+        assertEqual(mismatched, "", mismatched)
 
     def test_params_fixed_to_floating_point_different_config_for_layers(self):
         """
@@ -219,7 +218,7 @@ class SecureAggregatorTest(unittest.TestCase):
         for name, p in model.named_parameters():
             if name == "fc1.weight" or name == "fc1.bias":
                 # 832.8 / 30 = 27.76
-                self.assertTrue(torch.allclose(p, torch.tensor(27.76), rtol=1e-10))
+                assertTrue(torch.allclose(p, torch.tensor(27.76), rtol=1e-10))
             if name == "fc2.weight" or name == "fc2.bias":
                 # 832.8 / 80 = 10.41
-                self.assertTrue(torch.allclose(p, torch.tensor(10.41), rtol=1e-10))
+                assertTrue(torch.allclose(p, torch.tensor(10.41), rtol=1e-10))
