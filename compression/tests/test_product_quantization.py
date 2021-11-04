fbdab7b19a966c6055a3ed4cd5de4eeea7f37de9
Jesik Min <jesikmin@fb.com>
2021-11-04 11:21:36 -0700

    Update test_product_quantization as a PyTest
    Summary: as title
    
    Differential Revision: D32112705
    
    fbshipit-source-id: 255107a9561889adee3faf092de42bb451114458

--- compression/tests/test_product_quantization.py
+++ compression/tests/test_product_quantization.py
@@ -2,182 +2,183 @@
 # (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
 import torch
+from flsim.common.pytest_helper import assertEqual, assertRaises
 from flsim.compression.em import EM, EmptyClusterResolveError
 from flsim.compression.pq import PQ
-from libfb.py import testutil
 
 torch.manual_seed(0)
 
 
-class ProductQuantizationTest(testutil.BaseFacebookTestCase):
-    def setUp(self) -> None:
-        super().setUp()
+def test_em_constant() -> None:
+    """
+    We test our k-means (EM) algorithm on a dummy dataset
+    with 3 groups of 100 identical vectors and check that
+    the learnt centroids represent the 3 groups.
+    """
 
-    def test_em_constant(self) -> None:
-        """
-        We test our k-means (EM) algorithm on a dummy dataset
-        with 3 groups of 100 identical vectors and check that
-        the learnt centroids represent the 3 groups.
-        """
+    # define W as 100 * 3 vectors of dimension 2, the first 100 are
+    # filled with 1, the next 100 with 2 and the final 100 with 3
+    in_features = 2
+    out_features = 100
+    seed = torch.ones(out_features, in_features)
+    W = torch.cat([seed, 2 * seed, 3 * seed]).t()
 
-        # define W as 100 * 3 vectors of dimension 2, the first 100 are
-        # filled with 1, the next 100 with 2 and the final 100 with 3
-        in_features = 2
-        out_features = 100
-        seed = torch.ones(out_features, in_features)
-        W = torch.cat([seed, 2 * seed, 3 * seed]).t()
+    # we quantize with 2 centroids
+    em = EM(W, n_centroids=3)
+    em.learn()
 
-        # we quantize with 2 centroids
-        em = EM(W, n_centroids=3)
+    # we know the centroids, up to a permutation
+    seed = torch.ones(in_features)
+    true_centroids = torch.stack([seed, 2 * seed, 3 * seed])
+    norm = (em.centroids.sort(dim=0).values - true_centroids).norm()
+    assertEqual(norm, 0)
+
+
+def test_em_error() -> None:
+    """
+    We try to cluster identical vectors with 3 centroids and check that
+    our program raises an error.
+    """
+
+    W = torch.ones(2, 500)
+    em = EM(W, n_centroids=2)
+    with assertRaises(EmptyClusterResolveError):
         em.learn()
 
-        # we know the centroids, up to a permutation
-        seed = torch.ones(in_features)
-        true_centroids = torch.stack([seed, 2 * seed, 3 * seed])
-        norm = (em.centroids.sort(dim=0).values - true_centroids).norm()
-        self.assertEqual(norm, 0)
 
-    def test_em_error(self) -> None:
-        """
-        We try to cluster identical vectors with 3 centroids and check that
-        our program raises an error.
-        """
+def test_pq_linear_constant() -> None:
+    """
+    We check that PQ on dummy vectors of dimension 2 with a block size 2
+    in the linera case yields the expected centroids and that successively
+    quantizing and dequantizing amounts to the identity operation.
+    """
 
-        W = torch.ones(2, 500)
-        em = EM(W, n_centroids=2)
-        self.assertRaises(EmptyClusterResolveError, em.learn)
+    # define W as 100 * 3 vectors of dimension 2, the first 100 are
+    # filled with 1, the next 100 with 2 and the final 100 with 3
+    in_features = 2
+    out_features = 100
+    seed = torch.ones(out_features, in_features)
+    W = torch.cat([seed, 2 * seed, 3 * seed])
 
-    def test_pq_linear_constant(self) -> None:
-        """
-        We check that PQ on dummy vectors of dimension 2 with a block size 2
-        in the linera case yields the expected centroids and that successively
-        quantizing and dequantizing amounts to the identity operation.
-        """
+    # we quantize with block size 2 (each colun of W is split into 2)
+    pq = PQ(W.size(), max_block_size=2, num_codebooks=1, max_num_centroids=3)
+    centroids, assignments = pq.encode(W)
 
-        # define W as 100 * 3 vectors of dimension 2, the first 100 are
-        # filled with 1, the next 100 with 2 and the final 100 with 3
-        in_features = 2
-        out_features = 100
-        seed = torch.ones(out_features, in_features)
-        W = torch.cat([seed, 2 * seed, 3 * seed])
+    # we now the centroids, up to a permutation
+    seed = torch.ones(in_features)
+    true_centroids = torch.stack([seed, 2 * seed, 3 * seed])
+    norm = (centroids.sort(dim=0).values - true_centroids).norm()
 
-        # we quantize with block size 2 (each colun of W is split into 2)
-        pq = PQ(W.size(), max_block_size=2, num_codebooks=1, max_num_centroids=3)
-        centroids, assignments = pq.encode(W)
+    assertEqual(norm, 0)
 
-        # we now the centroids, up to a permutation
-        seed = torch.ones(in_features)
-        true_centroids = torch.stack([seed, 2 * seed, 3 * seed])
-        norm = (centroids.sort(dim=0).values - true_centroids).norm()
+    # we should recover exactly W
+    diff = (pq.decode(centroids, assignments) - W).norm()
+    assertEqual(diff, 0)
 
-        self.assertEqual(norm, 0)
 
-        # we should recover exactly W
-        diff = (pq.decode(centroids, assignments) - W).norm()
-        self.assertEqual(diff, 0)
+def test_pq_conv_constant() -> None:
+    """
+    We check that PQ on dummy vectors of dimension 9 with a block size 9
+    in the convolutional case yields the expected centroids and that
+    successively quantizing and dequantizing amounts to the identity operation.
+    """
 
-    def test_pq_conv_constant(self) -> None:
-        """
-        We check that PQ on dummy vectors of dimension 9 with a block size 9
-        in the convolutional case yields the expected centroids and that
-        successively quantizing and dequantizing amounts to the identity operation.
-        """
+    # define W as 10 * 5 * 3 vectors of dimension 9, the first 100 are
+    # filled with 1, the next 100 with 2 and the final 100 with 3
+    in_features = 10
+    out_features = 5
+    kernel_size = 3
+    seed = torch.ones(out_features, in_features, kernel_size, kernel_size)
+    W = torch.cat([seed, 2 * seed, 3 * torch.ones(4, 10, 3, 3)])
 
-        # define W as 10 * 5 * 3 vectors of dimension 9, the first 100 are
-        # filled with 1, the next 100 with 2 and the final 100 with 3
-        in_features = 10
-        out_features = 5
-        kernel_size = 3
-        seed = torch.ones(out_features, in_features, kernel_size, kernel_size)
-        W = torch.cat([seed, 2 * seed, 3 * torch.ones(4, 10, 3, 3)])
+    # we quantize with block size 2 (each colun of W is split into 2)
+    pq = PQ(W.size(), max_block_size=9, num_codebooks=1, max_num_centroids=3)
+    centroids, assignments = pq.encode(W)
 
-        # we quantize with block size 2 (each colun of W is split into 2)
-        pq = PQ(W.size(), max_block_size=9, num_codebooks=1, max_num_centroids=3)
-        centroids, assignments = pq.encode(W)
+    # we now the centroids, up to a permutation
+    seed = torch.ones(kernel_size * kernel_size)
+    true_centroids = torch.stack([seed, 2 * seed, 3 * seed])
+    norm = (centroids.sort(dim=0).values - true_centroids).norm()
+    assertEqual(norm, 0)
 
-        # we now the centroids, up to a permutation
-        seed = torch.ones(kernel_size * kernel_size)
-        true_centroids = torch.stack([seed, 2 * seed, 3 * seed])
-        norm = (centroids.sort(dim=0).values - true_centroids).norm()
-        self.assertEqual(norm, 0)
+    # we should recover exactly W
+    diff = (pq.decode(centroids, assignments) - W).norm()
+    assertEqual(diff, 0)
 
-        # we should recover exactly W
-        diff = (pq.decode(centroids, assignments) - W).norm()
-        self.assertEqual(diff, 0)
 
-    def test_pq_many_dicts(self) -> None:
-        """
-        We check that PQ on dummy vectors of dimension 4 with a block size 2
-        and two learnt codebooks in the linear case yields the expected centroids
-        (6 in total, 3 per codebook) and that successively quantizing and
-        dequantizing amounts to the identity operation.
-        """
+def test_pq_many_dicts() -> None:
+    """
+    We check that PQ on dummy vectors of dimension 4 with a block size 2
+    and two learnt codebooks in the linear case yields the expected centroids
+    (6 in total, 3 per codebook) and that successively quantizing and
+    dequantizing amounts to the identity operation.
+    """
 
-        # define W as 300 vectors of dimension 4, the first 100 are filled with
-        # 1, the next 100 with 2 and the final 100 with 3
-        in_features = 4
-        out_features = 100
-        seed = torch.ones(out_features, in_features)
-        W = torch.cat([seed, 2 * seed, 3 * seed])
+    # define W as 300 vectors of dimension 4, the first 100 are filled with
+    # 1, the next 100 with 2 and the final 100 with 3
+    in_features = 4
+    out_features = 100
+    seed = torch.ones(out_features, in_features)
+    W = torch.cat([seed, 2 * seed, 3 * seed])
 
-        # we quantize with block size 2 (each colun of W is split into 2)
-        num_codebooks = 2
-        pq = PQ(
-            W.size(), max_block_size=2, num_codebooks=num_codebooks, max_num_centroids=3
-        )
-        centroids, assignments = pq.encode(W)
+    # we quantize with block size 2 (each colun of W is split into 2)
+    num_codebooks = 2
+    pq = PQ(
+        W.size(), max_block_size=2, num_codebooks=num_codebooks, max_num_centroids=3
+    )
+    centroids, assignments = pq.encode(W)
 
-        # we now the centroids, up to a permutation
-        seed = torch.ones(in_features // num_codebooks)
-        true_centroids = torch.stack(
-            [seed, seed, 2 * seed, 2 * seed, 3 * seed, 3 * seed]
-        )
-        norm = (centroids.sort(dim=0).values - true_centroids).norm()
+    # we now the centroids, up to a permutation
+    seed = torch.ones(in_features // num_codebooks)
+    true_centroids = torch.stack([seed, seed, 2 * seed, 2 * seed, 3 * seed, 3 * seed])
+    norm = (centroids.sort(dim=0).values - true_centroids).norm()
 
-        self.assertEqual(norm, 0)
+    assertEqual(norm, 0)
 
-        # we should recover exactly W
-        diff = (pq.decode(centroids, assignments) - W).norm()
-        self.assertEqual(diff, 0)
+    # we should recover exactly W
+    diff = (pq.decode(centroids, assignments) - W).norm()
+    assertEqual(diff, 0)
 
-    def test_pq_conv_block_size(self) -> None:
-        """
-        We test that the effective block size is 9 when providing a maximum
-        block size of 10 in the convolutional case with 1 input feature.
-        """
 
-        # define W as 10 * 5 * 3 vectors of dimension 9, the first 100 are
-        # filled with 1, the next 100 with 2 and the final 100 with 3
-        in_features = 1
-        out_features = 5
-        kernel_size = 3
-        seed = torch.ones(out_features, in_features, kernel_size, kernel_size)
-        W = torch.cat([seed, 2 * seed, 3 * seed])
+def test_pq_conv_block_size() -> None:
+    """
+    We test that the effective block size is 9 when providing a maximum
+    block size of 10 in the convolutional case with 1 input feature.
+    """
 
-        # we quantize with block size 2 (each colun of W is split into 2)
-        pq = PQ(W.size(), max_block_size=10, num_codebooks=1, max_num_centroids=3)
-        centroids, assignemnts = pq.encode(W)
+    # define W as 10 * 5 * 3 vectors of dimension 9, the first 100 are
+    # filled with 1, the next 100 with 2 and the final 100 with 3
+    in_features = 1
+    out_features = 5
+    kernel_size = 3
+    seed = torch.ones(out_features, in_features, kernel_size, kernel_size)
+    W = torch.cat([seed, 2 * seed, 3 * seed])
 
-        # centroids should have dimension 9, which is the largest acceptable block size
-        self.assertEqual(centroids.size(1), kernel_size * kernel_size)
+    # we quantize with block size 2 (each colun of W is split into 2)
+    pq = PQ(W.size(), max_block_size=10, num_codebooks=1, max_num_centroids=3)
+    centroids, assignemnts = pq.encode(W)
 
-    def test_pq_linear_n_centroids(self) -> None:
-        """
-        We check that PQ on 100 random vectors yields a number of centroids equal
-        to 16 when providing a larger max_num_centroids equal to 32. Indeed, we
-        require n_centroids to be less than n_subvectors // 4 and a power of two.
-        """
+    # centroids should have dimension 9, which is the largest acceptable block size
+    assertEqual(centroids.size(1), kernel_size * kernel_size)
 
-        # define W as 100 * 3 vectors of dimension 2, the first 100 are
-        # filled with 1, the next 100 with 2 and the final 100 with 3
-        in_features = 2
-        out_features = 100
-        W = torch.rand(out_features, in_features)
 
-        # we quantize with block size 2 (each colun of W is split into 2)
-        pq = PQ(W.size(), max_block_size=2, num_codebooks=1, max_num_centroids=32)
-        centroids, assignments = pq.encode(W)
+def test_pq_linear_n_centroids() -> None:
+    """
+    We check that PQ on 100 random vectors yields a number of centroids equal
+    to 16 when providing a larger max_num_centroids equal to 32. Indeed, we
+    require n_centroids to be less than n_subvectors // 4 and a power of two.
+    """
 
-        # we now the centroids, up to a permutation
-        n_centroids = centroids.size(0)
-        self.assertEqual(n_centroids, 16)
+    # define W as 100 * 3 vectors of dimension 2, the first 100 are
+    # filled with 1, the next 100 with 2 and the final 100 with 3
+    in_features = 2
+    out_features = 100
+    W = torch.rand(out_features, in_features)
+
+    # we quantize with block size 2 (each colun of W is split into 2)
+    pq = PQ(W.size(), max_block_size=2, num_codebooks=1, max_num_centroids=32)
+    centroids, assignments = pq.encode(W)
+
+    # we now the centroids, up to a permutation
+    n_centroids = centroids.size(0)
+    assertEqual(n_centroids, 16)
