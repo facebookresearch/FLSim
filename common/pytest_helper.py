e6edfe6b96690b5ed549da9ad67d1792ef3e8375
Jesik Min <jesikmin@fb.com>
2021-11-04 13:25:09 -0700

    Update test_training_simulator as a PyTest (#4)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FLSim/pull/4
    
    as title
    
    Reviewed By: JohnlNguyen
    
    Differential Revision: D32116464
    
    fbshipit-source-id: 5c122f7f555404888a4318231e799b3b48231db9

--- common/pytest_helper.py
+++ common/pytest_helper.py
@@ -9,15 +9,39 @@ so that all FL-sim PyTests can reuse them easily.
 """
 from typing import Any
 
-import numpy as np
-
 
 def assertIsInstance(o: object, t: type) -> None:
     assert isinstance(o, t)
 
 
-def assertAlmostEqual(f1: float, f2: float, decimal=7) -> None:
-    np.testing.assert_almost_equal(f1, f2, decimal=decimal)
+def assertAlmostEqual(first, second, places=None, msg=None, delta=None):
+    """Assert that ``first`` and ``second`` is almost equal to each other.
+
+    The equality of ``first`` and ``second`` is determined in a similar way to
+    the ``assertAlmostEqual`` function of the standard library.
+    """
+    if delta is not None and places is not None:
+        raise TypeError("specify delta or places - not both")
+
+    msg: str = ""
+    diff = abs(second - first)
+    if delta is not None:
+        if diff <= delta:
+            return
+
+        msg = (
+            f"The difference between {first} and {second} is not within {delta} delta."
+        )
+    else:
+        if places is None:
+            places = 7
+
+        if round(diff, places) == 0:
+            return
+
+        msg = f"The difference between {first} and {second} is not within {places} decimal places."
+
+    raise AssertionError(msg)
 
 
 def assertEqual(object1: object, object2: object, e: object = None) -> None:
@@ -26,6 +50,14 @@ def assertEqual(object1: object, object2: object, e: object = None) -> None:
 
 def assertNotEqual(object1: object, object2: object, e: object = None) -> None:
     assert object1 != object2, e
+
+
+def assertLessEqual(object1, object2) -> None:
+    assert object1 <= object2
+
+
+def assertGreaterEqual(object1, object2) -> None:
+    assert object1 >= object2
 
 
 def assertIsNotNone(o: object) -> None:
