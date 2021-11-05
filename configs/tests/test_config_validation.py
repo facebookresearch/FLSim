0e33517544bd4bc73b028e16f6c24b1fa431a0f9
Jesik Min <jesikmin@fb.com>
2021-11-05 16:29:02 -0700

    Move config validation tests to Config folder and convert to a PyTest
    Summary: as title
    
    Reviewed By: JohnlNguyen
    
    Differential Revision: D32221579
    
    fbshipit-source-id: 29a1ea3d3f284a982d8b30f0ed284593441164a3

--- configs/tests/test_config_validation.py
+++ configs/tests/test_config_validation.py
@@ -2,18 +2,14 @@
 # (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
 import flsim.configs  # noqa
-import hydra
 import omegaconf
+from flsim.common.pytest_helper import assertRaises
 from flsim.utils.sample_model import DummyAlphabetFLModel
 from hydra.experimental import compose, initialize
 from hydra.utils import instantiate
-from libfb.py import testutil
 
 
-class ConfigValidation(testutil.BaseFacebookTestCase):
-    def setUp(self) -> None:
-        super().setUp()
-
+class TestConfigValidation:
     def test_throw_exception_on_missing_field(self) -> None:
         with initialize(config_path=None):
             cfg = compose(
@@ -26,10 +22,7 @@ class ConfigValidation(testutil.BaseFacebookTestCase):
                     "+staleness_weight@trainer.async_weight.staleness_weight=base_threshold_staleness_weight",
                 ],
             )
-        with self.assertRaises(
-            (
-                omegaconf.errors.MissingMandatoryValue,  # with Hydra 1.1
-                hydra.errors.HydraException,  # with Hydra 1.0
-            ),
+        with assertRaises(
+            omegaconf.errors.MissingMandatoryValue,  # with Hydra 1.1
         ):
             instantiate(cfg.trainer, model=DummyAlphabetFLModel(), cuda_enabled=False)
