52fbab212f29676066670ea971cf2414ec043bfe
Jessica Zhao <jessicazhao@fb.com>
2021-11-08 11:30:49 -0800

    Change git clone command to use HTTPS
    Summary:
    ## What?
    - Use HTTPS instead of SSH to get data for tutorial
    
    ## Why?
    - Does not require SSH setup
    
    Reviewed By: karthikprasad
    
    Differential Revision: D32223264
    
    fbshipit-source-id: ceb972fb3e6677b60c6b63e5c42b6a6a5dc70fe1

--- tutorials/get_data.sh
+++ tutorials/get_data.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone git@github.com:TalwalkarLab/leaf.git
+git clone https://github.com/TalwalkarLab/leaf.git
 cd leaf/data/sent140 || exit
 ./preprocess.sh --sf 0.01 -s niid -t 'user' --tf 0.90 -k 1 --spltseed 1
