9cc616e3da77847e84d4b5d5a2f0c5162cf4ce04
Jesik Min <jesikmin@fb.com>
2021-11-10 09:30:50 -0800

    Move UserUpdateClipper test to correct directory and convert it into a PyTest (#20)
    Summary:
    Pull Request resolved: https://github.com/facebookresearch/FLSim/pull/20
    
    as title
    
    Reviewed By: JohnlNguyen
    
    Differential Revision: D32306169
    
    fbshipit-source-id: 4ec4268c4a5cf3e33409404462c0416dfe514e19

--- privacy/tests/test_user_update_clipper.py
+++ privacy/tests/test_user_update_clipper.py
@@ -1,35 +1,39 @@
 #!/usr/bin/env python3
 # (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
-import unittest
-
+import pytest
 import torch
+from flsim.common.pytest_helper import (
+    assertAlmostEqual,
+    assertTrue,
+    assertEqual,
+    assertFalse,
+)
 from flsim.privacy.user_update_clip import UserUpdateClipper
 from flsim.tests import utils
 
 
-class UserUpdateClipperTest(unittest.TestCase):
-    def setUp(self) -> None:
-        super().setUp()
-        self.user_update_clipper = UserUpdateClipper()
-        self._init_original_model_diff()
+@pytest.fixture(scope="class")
+def prepare_user_update_clipper_test(request):
+    request.cls.user_update_clipper = UserUpdateClipper()
 
-    def _init_original_model_diff(self):
-        def _init_user_model(param_value):
-            user_model = utils.TwoFC()
-            user_model.fill_all(param_value)  # fill all parameters with a number
-            return user_model
+    def _init_user_model(param_value):
+        user_model = utils.TwoFC()
+        user_model.fill_all(param_value)  # fill all parameters with a number
+        return user_model
 
-        self.previous_user_model = _init_user_model(param_value=6.0)
-        self.updated_user_model = _init_user_model(param_value=7.0)
-        self.original_model_diff = self.user_update_clipper.calc_model_diff(
-            self.updated_user_model, self.previous_user_model
-        )
+    request.cls.previous_user_model = _init_user_model(param_value=6.0)
+    request.cls.updated_user_model = _init_user_model(param_value=7.0)
+    request.cls.original_model_diff = request.cls.user_update_clipper.calc_model_diff(
+        request.cls.updated_user_model, request.cls.previous_user_model
+    )
+    request.cls.original_model_diff_params = [
+        p for p in request.cls.original_model_diff.parameters() if p.requires_grad
+    ]
 
-        self.original_model_diff_params = [
-            p for p in self.original_model_diff.parameters() if p.requires_grad
-        ]
 
+@pytest.mark.usefixtures("prepare_user_update_clipper_test")
+class TestUserUpdateClipper:
     def _init_clipped_model_diff(self, max_norm):
         clipped_model_diff = utils.TwoFC()
         clipped_model_diff.load_state_dict(self.original_model_diff.state_dict())
@@ -44,17 +48,17 @@ class UserUpdateClipperTest(unittest.TestCase):
         clip_factor = self.user_update_clipper._calc_clip_factor(
             max_norm=5, per_user_norm=10
         )
-        self.assertAlmostEquals(clip_factor, 0.5, places=3)
+        assertAlmostEqual(clip_factor, 0.5, places=3)
 
         clip_factor = self.user_update_clipper._calc_clip_factor(
             max_norm=1, per_user_norm=1
         )
-        self.assertAlmostEquals(clip_factor, 1, places=3)
+        assertAlmostEqual(clip_factor, 1, places=3)
 
         clip_factor = self.user_update_clipper._calc_clip_factor(
             max_norm=2, per_user_norm=0.8
         )
-        self.assertAlmostEquals(clip_factor, 1, places=3)
+        assertAlmostEqual(clip_factor, 1, places=3)
 
     def test_calc_user_update_norm(self):
         """
@@ -65,17 +69,13 @@ class UserUpdateClipperTest(unittest.TestCase):
         model_params = model.parameters()
         # norm = sqrt(21*2^2)=sqrt(84)=9.16515138991168
         norm = self.user_update_clipper._calc_norm(model_params)
-        self.assertTrue(
-            torch.allclose(norm, torch.tensor(9.16515138991168), rtol=1e-06)
-        )
+        assertTrue(torch.allclose(norm, torch.tensor(9.16515138991168), rtol=1e-06))
 
         model.fill_all(1.0)
         model_params = model.parameters()
         # norm = sqrt(21*1^2)=sqrt(21)=4.58257569495584
         norm = self.user_update_clipper._calc_norm(model_params)
-        self.assertTrue(
-            torch.allclose(norm, torch.tensor(4.58257569495584), rtol=1e-06)
-        )
+        assertTrue(torch.allclose(norm, torch.tensor(4.58257569495584), rtol=1e-06))
 
     def test_clipped_updates_are_smaller(self):
         """
@@ -85,7 +85,7 @@ class UserUpdateClipperTest(unittest.TestCase):
 
         # assert the parameters of model_diff are all = (7 - 6 = 1)
         for p in self.original_model_diff_params:
-            self.assertTrue(torch.allclose(p.float(), torch.tensor(1.0)))
+            assertTrue(torch.allclose(p.float(), torch.tensor(1.0)))
 
         clipped_model_diff = self._init_clipped_model_diff(0.0003)
         clipped_model_diff_params = [
@@ -94,7 +94,7 @@ class UserUpdateClipperTest(unittest.TestCase):
         for original, clipped in zip(
             self.original_model_diff_params, clipped_model_diff_params
         ):
-            self.assertTrue(torch.all(original.gt(clipped)))
+            assertTrue(torch.all(original.gt(clipped)))
 
     def test_clipped_user_updates_non_zero(self):
         """
@@ -106,7 +106,7 @@ class UserUpdateClipperTest(unittest.TestCase):
         ]
         for clipped in clipped_model_diff_params:
             allzeros = torch.zeros_like(clipped)
-            self.assertFalse(torch.allclose(clipped, allzeros))
+            assertFalse(torch.allclose(clipped, allzeros))
 
     def test_clipping_to_high_value_does_not_clip(self):
         """
@@ -117,4 +117,4 @@ class UserUpdateClipperTest(unittest.TestCase):
         mismatched = utils.verify_models_equivalent_after_training(
             self.original_model_diff, clipped_model_diff
         )
-        self.assertEqual(mismatched, "")
+        assertEqual(mismatched, "")
