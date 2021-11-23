44b599c18b1462da2d649fce6089921026162ced
John Nguyen <ngjhn@fb.com>
2021-11-23 10:15:26 -0800

    Move test_async_user_selector to pytest
    Summary: as titled
    
    Reviewed By: sayanghosh
    
    Differential Revision: D32576571
    
    fbshipit-source-id: 9be0b6b14533e61608c2327a7d068a3ce560207b

--- utils/tests/test_async_user_selector.py
+++ utils/tests/test_async_user_selector.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python3
 # (c) Facebook, Inc. and its affiliates. Confidential and proprietary.
 
+import pytest
+from flsim.common.pytest_helper import assertEqual, assertTrue
 from flsim.data.data_provider import FLDataProviderFromList
 from flsim.utils.async_trainer.async_user_selector import (
     RandomAsyncUserSelector,
     RoundRobinAsyncUserSelector,
 )
 from flsim.utils.sample_model import MockFLModel
-from libfb.py import testutil
 
 
-class AsyncUserSelectorUtilsTest(testutil.BaseFacebookTestCase):
-    def setUp(self) -> None:
-        super().setUp()
-        self.num_users = 20
-        self.num_trials = 100
+@pytest.fixture
+def num_users():
+    return 20
 
-    def test_random_user_selector(self):
+
+@pytest.fixture
+def num_trials():
+    return 100
+
+
+class TestAsyncUserSelectorUtils:
+    def test_random_user_selector(self, num_users, num_trials):
         # users are 0....n-1
         # number of examples per user: [1, 2, 3...., n-1, n]
-        num_examples_per_user = list(range(1, self.num_users + 1))
+        num_examples_per_user = list(range(1, num_users + 1))
         data = [
             [1] * num_example
-            for num_example, _ in zip(num_examples_per_user, range(self.num_users))
+            for num_example, _ in zip(num_examples_per_user, range(num_users))
         ]
         data_provider = FLDataProviderFromList(
             train_user_list=data,
@@ -31,26 +37,24 @@ class AsyncUserSelectorUtilsTest(testutil.BaseFacebookTestCase):
             model=MockFLModel(),
         )
         random_user_selector = RandomAsyncUserSelector(data_provider=data_provider)
-        for _ in range(0, 100):
+        for _ in range(0, num_trials):
             random_user_info = random_user_selector.get_random_user()
             random_user, user_index = (
                 random_user_info.user_data,
                 random_user_info.user_index,
             )
-            self.assertTrue(user_index >= 0 and user_index < self.num_users)
-            self.assertEqual(random_user.num_examples(), user_index + 1)
+            assertTrue(user_index >= 0 and user_index < num_users)
+            assertEqual(random_user.num_examples(), user_index + 1)
 
-    def test_round_robin_user_selector(self):
+    def test_round_robin_user_selector(self, num_users, num_trials):
         # users are 0....n-1
-        # number of examples per user: [10, 20, 30...., 10*n-1, 10*n]
+        # number of examples per user: [10, num_users, 30...., 10*n-1, 10*n]
         multiplier = 10
-        num_examples_per_user = [
-            multiplier * i for i in list(range(1, self.num_users + 1))
-        ]
+        num_examples_per_user = [multiplier * i for i in list(range(1, num_users + 1))]
         round_robin_user_selector = RoundRobinAsyncUserSelector(num_examples_per_user)
         data = [
             [1] * num_example
-            for num_example, _ in zip(num_examples_per_user, range(self.num_users))
+            for num_example, _ in zip(num_examples_per_user, range(num_users))
         ]
         data_provider = FLDataProviderFromList(
             train_user_list=data,
@@ -61,11 +65,11 @@ class AsyncUserSelectorUtilsTest(testutil.BaseFacebookTestCase):
         round_robin_user_selector = RoundRobinAsyncUserSelector(
             data_provider=data_provider
         )
-        for num_trial in range(0, self.num_trials):
+        for num_trial in range(0, num_trials):
             random_user_info = round_robin_user_selector.get_random_user()
             random_user, user_index = (
                 random_user_info.user_data,
                 random_user_info.user_index,
             )
-            self.assertEqual(user_index, num_trial % self.num_users)
-            self.assertEqual(random_user.num_examples(), (user_index + 1) * multiplier)
+            assertEqual(user_index, num_trial % num_users)
+            assertEqual(random_user.num_examples(), (user_index + 1) * multiplier)
