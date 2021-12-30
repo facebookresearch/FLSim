c8548ab846fe098431d6477386c512b45252363e
Andres Suarez <asuarez@fb.com>
2021-12-30 14:20:10 -0800

    Update copyright headers from Facebook to Meta
    Reviewed By: karthikprasad
    
    Differential Revision: D33366913
    
    fbshipit-source-id: cbed6bf673d16a6f4f74d535542060894d1b82dd

--- examples/get_data.sh
+++ examples/get_data.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 #
-# This source code is licensed under the BSD-style license found in the
-# LICENSE file in the root directory of this source tree.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 git clone https://github.com/TalwalkarLab/leaf.git
 cd leaf/data/sent140 || exit
 ./preprocess.sh --sf 0.01 -s niid -t 'user' --tf 0.90 -k 1 --spltseed 1
