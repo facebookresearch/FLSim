Fix overflow calculation for SecAgg
Summary: We discovered a bug in reporting SecAgg overflows: we report evey x rounds, but the overflows are accumulated over those x rounds not divided by x. This diff fixes that

Reviewed By: pierrestock

Differential Revision: D37053211

fbshipit-source-id: e4546d8e8fba6f86db01e1e3e32468f186a27fdd

flsim/secure_aggregation/secure_aggregator.py
@@ -359,8 +359,9 @@ class SecureAggregator:
 
     def calc_avg_overflow_percentage(
         self,
-        num_users: int,
+        users_per_round: int,
         model: nn.Module,
+        report_rounds: int,
     ) -> Tuple[float, float]:
         """
         Calcualtes the percentage of average overflow over all model layers,
@@ -368,8 +369,11 @@ class SecureAggregator:
         overflow counters to make them ready for the next round.
 
         Args:
-            num_users: the total number of users in the system
+            users_per_round: the total number of users in one round
             model: the global model
+            report_rounds: the number of rounds during which the average
+                overflow is calculated (rounds elapsed between this reporting
+                and the previous one)
 
         Notes:
             The assumption here is that the model is always the same acorss
@@ -381,9 +385,11 @@ class SecureAggregator:
         convert_overflow_perc = sum(
             converter.get_convert_overflow(reset=True) * 100
             for converter in self.converters.values()
-        ) / (num_params * num_users)
+        ) / (num_params * users_per_round * report_rounds)
         aggregate_overflow_perc = (
-            self.get_aggregate_overflow(reset=True) * 100 / (num_params * num_users)
+            self.get_aggregate_overflow(reset=True)
+            * 100
+            / (num_params * users_per_round * report_rounds)
         )
         return convert_overflow_perc, aggregate_overflow_perc
 

flsim/trainers/sync_trainer.py
@@ -66,6 +66,7 @@ class SyncTrainer(FLTrainer):
             channel=self.channel,
         )
         self.clients = {}
+        self._last_report_round_after_aggregation = 0
 
     @classmethod
     def _set_defaults_in_cfg(cls, cfg):
@@ -464,6 +465,7 @@ class SyncTrainer(FLTrainer):
         clients: Iterable[Client],
         model: IFLModel,
         users_per_round: int,
+        report_rounds: int,
         metrics_reporter: Optional[IFLMetricsReporter],
     ) -> List[Metric]:
         """Calculates overflow metrics."""
@@ -475,7 +477,7 @@ class SyncTrainer(FLTrainer):
                 convert_overflow_perc,
                 aggregate_overflow_perc,
             ) = self.server.calc_avg_overflow_percentage(  # pyre-fixme
-                users_per_round, model
+                users_per_round, model, report_rounds
             )
             overflow_metrics: List[Metric] = Metric.from_args(
                 convert_overflow_percentage=convert_overflow_perc,
@@ -522,6 +524,10 @@ class SyncTrainer(FLTrainer):
             and self.cfg.report_train_metrics_after_aggregation
             and timeline.tick(1.0 / self.cfg.train_metrics_reported_per_epoch)
         ):
+            current_round = timeline.global_round_num()
+            report_rounds = current_round - self._last_report_round_after_aggregation
+            self._last_report_round_after_aggregation = current_round
+
             with torch.no_grad():
                 model.fl_get_module().eval()
                 for client in clients:
@@ -535,7 +541,7 @@ class SyncTrainer(FLTrainer):
                 clients, model, metrics_reporter
             )
             overflow_metrics = self._calc_overflow_metrics(
-                clients, model, users_per_round, metrics_reporter
+                clients, model, users_per_round, report_rounds, metrics_reporter
             )
 
             metrics_reporter.report_metrics(