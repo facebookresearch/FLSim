6e99e29adcfeb6368c933769b173dce324cbb273
Karthik Prasad <krp@fb.com>
2022-07-22 07:47:48 -0700

    add support for SimpleConveNet in celeba example
    Reviewed By: pierrestock
    
    Differential Revision: D38038113
    
    fbshipit-source-id: 1573ecffca80fa2e237c3cf5fdfb5058073ef2ef

--- examples/celeba_example.py
+++ examples/celeba_example.py
@@ -16,7 +16,7 @@ Before running this file, you need to download the dataset and partition the dat
     - Download the celebrity faces dataset from the same site. Place the images in a folder
        named `img_align_celeba` in the same folder as above.
 4. Run the pre-processing script:
-    - `./preprocess.sh --sf 0.01 -s niid -t 'user' --tf 0.90 -k 1 --spltseed 1`
+    - `./preprocess.sh --sf 1.0 -k 0 -iu 1 -s niid -t 'user' --tf 0.90 --spltseed 1`
 
 Typical usage example:
     python3 celeba_example.py --config-file configs/celeba_config.json
@@ -111,12 +111,12 @@ def build_data_provider(data_config):
         ]
     )
     train_dataset = CelebaDataset(
-        data_root="leaf/data/celeba/data/train/all_data_0_01_keep_1_train_9.json",
+        data_root="leaf/data/celeba/data/train/all_data_0_0_keep_0_train_9.json",
         image_root="leaf/data/celeba/data/raw/",
         transform=transform,
     )
     test_dataset = CelebaDataset(
-        data_root="leaf/data/celeba/data/test/all_data_0_01_keep_1_test_9.json",
+        data_root="leaf/data/celeba/data/test/all_data_0_0_keep_0_test_9.json",
         transform=transform,
         image_root=train_dataset.image_root,
     )
@@ -149,14 +149,71 @@ class Resnet18(nn.Module):
         return self.backbone(x)
 
 
+class SimpleConvNet(nn.Module):
+    r"""
+    Simple CNN model following architecture from
+    https://github.com/TalwalkarLab/leaf/blob/master/models/celeba/cnn.py#L19
+    and https://arxiv.org/pdf/1903.03934.pdf
+    """
+
+    def __init__(self, in_channels, num_classes, dropout_rate=0):
+        super(SimpleConvNet, self).__init__()
+        self.out_channels = 32
+        self.stride = 1
+        self.padding = 2
+        self.layers = []
+        in_dim = in_channels
+        for _ in range(4):
+            self.layers.append(
+                nn.Conv2d(in_dim, self.out_channels, 3, self.stride, self.padding)
+            )
+            in_dim = self.out_channels
+        self.layers = nn.ModuleList(self.layers)
+
+        self.gn_relu = nn.Sequential(
+            nn.GroupNorm(self.out_channels, self.out_channels, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+        num_features = (
+            self.out_channels
+            * (self.stride + self.padding)
+            * (self.stride + self.padding)
+        )
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc = nn.Linear(num_features, num_classes)
+
+    def forward(self, x):
+        for conv in self.layers:
+            x = self.gn_relu(conv(x))
+
+        x = x.view(-1, self.num_flat_features(x))
+        x = self.fc(self.dropout(x))
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:]  # all dimensions except the batch dimension
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
 def main_worker(
     trainer_config,
     data_config,
+    model_config,
     use_cuda_if_available: bool = True,
     distributed_world_size: int = 1,
 ) -> None:
     data_provider = build_data_provider(data_config)
-    model = Resnet18(num_classes=2)
+    model = (
+        Resnet18(num_classes=2)
+        if model_config.use_resnet
+        else SimpleConvNet(
+            in_channels=3, num_classes=2, dropout_rate=model_config.dropout
+        )
+    )
 
     cuda_enabled = torch.cuda.is_available() and use_cuda_if_available
     device = torch.device(f"cuda:{0}" if cuda_enabled else "cpu")
@@ -169,7 +226,12 @@ def main_worker(
 
     final_model, eval_score = trainer.train(
         data_provider=data_provider,
-        metrics_reporter=MetricsReporter([Channel.TENSORBOARD, Channel.STDOUT]),
+        metrics_reporter=MetricsReporter(
+            [Channel.TENSORBOARD, Channel.STDOUT],
+            target_eval=model_config.target_eval,
+            window_size=model_config.window_size,
+            average_type=model_config.average_type,
+        ),
         num_total_users=data_provider.num_train_users(),
         distributed_world_size=distributed_world_size,
     )
@@ -183,14 +245,12 @@ def main_worker(
 def run(cfg: DictConfig) -> None:
     print(OmegaConf.to_yaml(cfg))
 
-    trainer_config = cfg.trainer
-    data_config = cfg.data
-
     main_worker(
-        trainer_config,
-        data_config,
-        cfg.use_cuda_if_available,
-        cfg.distributed_world_size,
+        trainer_config=cfg.trainer,
+        data_config=cfg.data,
+        model_config=cfg.model,
+        use_cuda_if_available=cfg.use_cuda_if_available,
+        distributed_world_size=cfg.distributed_world_size,
     )
 
 
